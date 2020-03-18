894578527fbf660d44b98448fc99d9c59317386d
Michaela Kulasekara <mkulase@siue.edu>
2020-03-18 16:12:30 -0500

    5 EQ bands showing percent gains, added (non-functional) upload audio button

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-
+        self.setDefaults()
         self.loadSettings()
 
     def loadSettings(self):
@@ -51,11 +51,52 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_MainWindow):
         settings.endGroup()
 
 
+    def setDefaults(self):
+        self.gainSlider_band1.setValue(100)
+        self.gainSlider_band2.setValue(100)
+        self.gainSlider_band3.setValue(100)
+        self.gainSlider_band4.setValue(100)
+        self.gainSlider_band5.setValue(100)
+        self.gainSlider_band1.setMinimum(0)
+        self.gainSlider_band1.setMaximum(100)
+        self.gainSlider_band2.setMinimum(0)
+        self.gainSlider_band2.setMaximum(100)
+        self.gainSlider_band3.setMinimum(0)
+        self.gainSlider_band3.setMaximum(100)
+        self.gainSlider_band4.setMinimum(0)
+        self.gainSlider_band4.setMaximum(100)
+        self.gainSlider_band5.setMinimum(0)
+        self.gainSlider_band5.setMaximum(100)
+        self.gainLabel1.setText('Band 1 Gain: 100%')
+        self.gainLabel2.setText('Band 2 Gain: 100%')
+        self.gainLabel3.setText('Band 3 Gain: 100%')
+        self.gainLabel4.setText('Band 4 Gain: 100%')
+        self.gainLabel5.setText('Band 5 Gain: 100%')
+
+
     @pyqtSlot(int)
-    def on_sliceSlider_valueChanged(self, value):
-        print('slice number: %d' % value)
-        self.sliceWidgetBMode.sliceNumber = value
-        self.sliceWidgetIQ.sliceNumber = value
-        self.labelSlice.setText('Slice: %d' % value)
-        self.sliceWidgetBMode.updateFigure(self.roiDataList, self.landMarkViewNumber, self.ptSettings)
-        self.sliceWidgetIQ.updateFigure(self.roiDataList, self.landMarkViewNumber, self.ptSettings)
+    def on_gainSlider_band1_valueChanged(self, value):
+        print('Band 1 Gain: %d' % value + '%')
+        self.gainLabel1.setText('Band 1 Gain: %d' % value + '%')
+
+
+    @pyqtSlot(int)
+    def on_gainSlider_band2_valueChanged(self, value):
+        print('Band 2 Gain: %d' % value + '%')
+        self.gainLabel2.setText('Band 2 Gain: %d' % value + '%')
+
+    @pyqtSlot(int)
+    def on_gainSlider_band3_valueChanged(self, value):
+        print('Band 3 Gain: %d' % value + '%')
+        self.gainLabel3.setText('Band 3 Gain: %d' % value + '%')
+
+    @pyqtSlot(int)
+    def on_gainSlider_band4_valueChanged(self, value):
+        print('Band 4 Gain: %d' % value + '%')
+        self.gainLabel4.setText('Band 4 Gain: %d' % value + '%')
+
+    @pyqtSlot(int)
+    def on_gainSlider_band5_valueChanged(self, value):
+        print('Band 5 Gain: %d' % value + '%')
+        self.gainLabel5.setText('Band 5 Gain: %d' % value + '%')
+
